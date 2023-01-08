b264ba4e39a8a6b583d604afead85c2dab0d0d37
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-01-07 23:58:39 -0800

    fix formatting

--- print_nuc.py
+++ print_nuc.py
@@ -1,11 +1,10 @@
 import sys
+
 x = 0
 while x <= 20:
-    
+
     print("\t \033[1;32m A \033[1;31m T \033[1;34m C \033[1;33m G \n")
-    
 
     x += 1
-#reset terminal:
-#print('\033c')
-
+# reset terminal:
+# print('\033c')
